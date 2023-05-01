c3c39d4ac77562e1ff85e24bea9e913eaa386f9a
tsh <tsh@qq.com>
2023-05-01 19:33:03 +0800

    System call tracing

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,4 +104,5 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int trace_mask;              // 用于跟踪子进程的系统调用
 };
